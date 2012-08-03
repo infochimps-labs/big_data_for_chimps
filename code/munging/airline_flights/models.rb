049ddc432cd9805673dbc8179321e051a4df642a
Philip (flip) Kromer <flip@infochimps.org>
2012-08-03 12:36:29 -0500

    airline flights

---
+++ code/munging/airline_flights/models.rb
@@ -0,0 +1,9 @@
+require 'gorillib/model'
+require 'gorillib/model/factories'
+require 'gorillib/model/serialization'
+require 'gorillib/model/serialization/csv'
+
+require_relative './airline'
+require_relative './airport'
+require_relative './route'
+require_relative './flight'
