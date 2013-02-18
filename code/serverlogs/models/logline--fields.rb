f9d3f4840f6350d3ff9397a7699ea9399df055b6
Philip (flip) Kromer <flip@infochimps.org>
2013-02-17 18:26:24 -0600

    working on serverlog content

---
+++ code/serverlogs/models/logline--fields.rb
@@ -0,0 +1,18 @@
+class Logline
+  include Gorillib::Model
+  include Gorillib::Model::PositionalFields
+
+  field :ip_address,    String
+  field :rfc_1413,      String
+  field :userid,        String
+  #
+  field :requested_at,  Time
+  field :http_method,   String
+  field :path,          String
+  field :protocol,      String
+  field :response_code, Integer
+  field :bytesize,      Integer, blankish: ['', nil, '-']
+  field :referer,       String
+  field :user_agent,    String
+
+end
