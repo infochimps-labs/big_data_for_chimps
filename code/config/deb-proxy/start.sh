3c16c194a902872df202b11580b245d0f3e03f09
Philip (flip) Kromer <flip@infochimps.org>
2014-11-10 11:30:59 -0600

    Moving code to own repo

--- code/config/deb-proxy/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir /deb-proxy/cache /deb-proxy/log
-chown -R proxy.proxy /deb-proxy
-. /usr/share/squid-deb-proxy/init-common.sh
-pre_start
-/usr/sbin/squid3 -N -f /etc/squid-deb-proxy/squid-deb-proxy.conf
